261a58654b4208613c5617e09c7d01d17d3688b7
Blackfianit <divashinin@mail.ru>
2021-11-22 22:35:57 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Дмитрий Ивашинин on 22.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
